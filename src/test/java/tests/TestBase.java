cd1efc2b13f3469f521ec4d613ad63338488b2fb
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-05-16 05:58:53 +0700

    hw done

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -37,6 +37,6 @@ public class TestBase {
         Attach.pageSource();
 
         step("Verify content found", Selenide::closeWebDriver);
-        //Attach.video(sessionId);
+        Attach.video(sessionId);
     }
 }
